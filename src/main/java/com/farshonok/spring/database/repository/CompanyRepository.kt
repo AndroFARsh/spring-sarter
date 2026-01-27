bdc25af4ac0f12dcf780764b228fcfa0f121f325
Anton Kukhlevskyi <anton.kukhlevskyi@ext.airbnb.com>
2026-01-26 17:40:27 -0800

    Explicitly use named query

--- src/main/java/com/farshonok/spring/database/repository/CompanyRepository.kt
+++ src/main/java/com/farshonok/spring/database/repository/CompanyRepository.kt
@@ -2,14 +2,16 @@ package com.farshonok.spring.database.repository
 
 import com.farshonok.spring.database.entities.Company
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.Optional
 
 interface CompanyRepository : JpaRepository<Company, Int> {
-    fun findById(id: Int, properties: Map<String, String> = emptyMap()): Optional<Company>
-
+//    fun findById(id: Int, properties: Map<String, String> = emptyMap()): Optional<Company>
 //    fun delete(company: Company)
 //    fun deleteById(id: Int)
 
+    // Using NamedQuery from Company entity
+    @Query(name = "Company.findByName")
     // Can return Optional, Company?, Future
     fun findByName(name: String): Optional<Company>
 
