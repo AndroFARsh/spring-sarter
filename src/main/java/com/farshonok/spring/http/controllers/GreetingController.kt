e8a197cc17619c58fd526ac6cfc113923b540ca4
Anton Kukhlevskyi <anton.kukhlevskyi@ext.airbnb.com>
2026-02-09 12:10:52 -0800

    Simple moddel experiment, Session and Request attributes

--- src/main/java/com/farshonok/spring/http/controllers/GreetingController.kt
+++ src/main/java/com/farshonok/spring/http/controllers/GreetingController.kt
@@ -1,7 +1,7 @@
 package com.farshonok.spring.http.controllers
 
+import com.farshonok.spring.dto.PersonaInfo
 import jakarta.servlet.http.HttpServletRequest
-import jakarta.websocket.server.PathParam
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,14 +10,31 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.SessionAttribute
+import org.springframework.web.bind.annotation.SessionAttributes
 import org.springframework.web.servlet.ModelAndView
+import java.time.LocalDate
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes(value = ["user"])
 class GreetingController {
 
+    @RequestMapping("/hello")
+    fun hello1(
+//        request: HttpServletRequest,
+        mv: ModelAndView
+    ) : ModelAndView {
+        val p = PersonaInfo("Ivan", "Inanov", LocalDate.now())
+//        request.session.setAttribute("user", p) // sessionScope
+//        request.setAttribute("user", p) // requestScope
+        mv.addObject("user", p)
+        mv.viewName = "greeting/hello"
+        return mv
+    }
+
     @RequestMapping("/hello/{id}", method = [RequestMethod.GET])
-    fun hello(request: HttpServletRequest,
+    fun hello1(request: HttpServletRequest,
               @RequestParam age: Int,
               @RequestHeader("accept") accept: String,
               @CookieValue("JSESSIONID") jsessionId: String,
@@ -34,7 +51,8 @@ class GreetingController {
     }
 
     @GetMapping("/bye")
-    fun bye(mv: ModelAndView): ModelAndView {
+    fun bye(mv: ModelAndView, @SessionAttribute("user") user: PersonaInfo): ModelAndView {
+        // call /hello to set user into sessionScope first or exception
         mv.viewName = "greeting/bye"
         return mv
     }
