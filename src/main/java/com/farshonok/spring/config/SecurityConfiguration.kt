38d197a7c7abde3bda733ecbb57c174706467ccb
Anton Kukhlevskyi <anton.kukhlevskyi@ext.airbnb.com>
2026-03-17 17:30:17 -0700

    http basic auth experiment

--- src/main/java/com/farshonok/spring/config/SecurityConfiguration.kt
+++ src/main/java/com/farshonok/spring/config/SecurityConfiguration.kt
@@ -2,8 +2,10 @@ package com.farshonok.spring.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 
 @Configuration
 class SecurityConfiguration {
@@ -21,12 +23,13 @@ class SecurityConfiguration {
                 .anyRequest()
                 .authenticated()
         }
-        .formLogin { formLogin ->
-            formLogin // Use the FormLoginConfigurer
-                .loginPage("/login") // Specify the custom login page URL
-                .defaultSuccessUrl("/users", true) // Redirect to /home after successful login
-                .failureUrl("/login?error=true") // Redirect to /login?error=true on failure
-                .permitAll()
-        }
+        .httpBasic(Customizer.withDefaults())
+//        .formLogin { formLogin ->
+//            formLogin // Use the FormLoginConfigurer
+//                .loginPage("/login") // Specify the custom login page URL
+//                .defaultSuccessUrl("/users", true) // Redirect to /home after successful login
+//                .failureUrl("/login?error=true") // Redirect to /login?error=true on failure
+//                .permitAll()
+//        }
         .build()
 }
